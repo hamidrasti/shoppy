e4f443569f16c1c1f7259510f0dcb03b5d5601f4
hamraa <raasti.hamid@gmail.com>
2022-04-20 22:04:58 +0430

    configure debug toolbar and api schema urls

--- shoppy/urls.py
+++ shoppy/urls.py
@@ -1,6 +1,22 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+admin.site.site_header = 'Shoppy Admin'
+admin.site.index_title = 'Admin'
+
+api_urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(api_urlpatterns)),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
